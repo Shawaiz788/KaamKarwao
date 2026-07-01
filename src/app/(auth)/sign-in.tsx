8f150a4ae29b55ca2abd68c24658842ba5b3ce7e
Shawaiz Ali Rehman <shawaiali788@gmail.com>
2026-07-01 09:12:21 +0800

    Added:Screens-expo router navigation folder structure created

---
+++ src/app/(auth)/sign-in.tsx
@@ -0,0 +1,93 @@
+import { StatusBar } from 'expo-status-bar';
+import {
+    StyleSheet,
+    Text,
+    View,
+    KeyboardAvoidingView,
+    Platform,
+} from 'react-native';
+import CustomInput from '@/components/CustomInput';
+import CustomButton from '@/components/CustomButton';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Link } from 'expo-router';
+
+const signInSchema = z.object({
+    email: z.string({ message: 'Email is required' }).email('Invalid email'),
+    password: z
+        .string({ message: 'Password is required' })
+        .min(8, 'Password should be at least 8 characters long'),
+});
+
+type SignInFields = z.infer<typeof signInSchema>;
+
+export default function SignInScreen() {
+    const { control, handleSubmit, formState: { errors } } = useForm<SignInFields>({
+        resolver: zodResolver(signInSchema)
+    });
+
+    const OnSignInPress = (data: SignInFields) => {
+        console.log('SignIn:', data);
+    };
+
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={styles.container}
+        >
+            <Text style={styles.title}>Sign in</Text>
+
+            <View style={styles.form}>
+                <CustomInput
+                    placeholder='Email'
+                    name='email'
+                    autoFocus
+                    autoCapitalize='none'
+                    keyboardType='email-address'
+                    autoComplete='email'
+                    control={control}
+                />
+                <CustomInput
+                    placeholder='Password'
+                    secureTextEntry
+                    control={control}
+                    name='password'
+                />
+            </View>
+            <CustomButton
+                text='Sign in'
+                onPress={handleSubmit(OnSignInPress)}
+            />
+
+            <Link href='/sign-up' style={styles.link}>
+                Don't have an account? Sign up
+            </Link>
+
+            <StatusBar style='auto' />
+        </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+        gap: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+    form: {
+        gap: 5,
+        width: '100%',
+        alignItems: 'center',
+    }, link: {
+        color: '#4353FD',
+        fontWeight: '600',
+    },
+});
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,85 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import {
-  StyleSheet,
-  Text,
-  View,
-  KeyboardAvoidingView,
-  Platform,
-} from 'react-native';
-import CustomInput from '@/components/CustomInput';
-import CustomButton from '@/components/CustomButton';
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { Text, StyleSheet, View } from 'react-native';
+import { Link } from 'expo-router';
 
-const signInSchema = z.object({
-  email: z.string({ message: 'Email is required' }).email('Invalid email'),
-  password: z
-    .string({ message: 'Password is required' })
-    .min(8, 'Password should be at least 8 characters long'),
-});
-
-type SignInFields = z.infer<typeof signInSchema>;
-
-export default function App() {
-  const { control, handleSubmit, formState: { errors } } = useForm<SignInFields>({
-    resolver: zodResolver(signInSchema)
-  });
-
-  const OnSignInPress = (data: SignInFields) => {
-    console.log('SignIn:', data);
-  };
-
+export default function Index() {
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={styles.container}
-    >
-      <Text style={styles.title}>Sign in</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome screen</Text>
 
-      <View style={styles.form}>
-        <CustomInput
-          placeholder='Email'
-          name='email'
-          autoFocus
-          autoCapitalize='none'
-          keyboardType='email-address'
-          autoComplete='email'
-          control={control}
-        />
-        <CustomInput
-          placeholder='Password'
-          secureTextEntry
-          control={control}
-          name='password'
-        />
-      </View>
-      <CustomButton
-        text='Sign in'
-        onPress={handleSubmit(OnSignInPress)}
-      />
-
-      <StatusBar style='auto' />
-    </KeyboardAvoidingView>
+      <Link href="/sign-in">Go to sign in</Link>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
+    alignItems: 'center',
     gap: 20,
   },
   title: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: 'bold',
   },
-  form: {
-    gap: 5,
-    width: '100%',
-    alignItems: 'center',
-  }
 });
